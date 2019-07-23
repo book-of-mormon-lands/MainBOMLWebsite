012595e847ee1cd38e806a8ceb89e06b7975d8ef
Benjamin Despain <bendespain@hotmail.com>
2019-07-23 14:50:00 -0600

    open element add

---
+++ OpenElement/BookofMormonLandsOrg/double-blocs(var).php
@@ -0,0 +1,4 @@
+<?php
+$OESiteUploadDirectory = "Upload";
+$OEConfWETextBox = '{"WE4bbed1ca32":{"InputName":"WE4bbed1ca32"},"WE70e4d8bd77":{"InputName":"WE70e4d8bd77"}}';
+$OEConfWETextArea = '{"WE78e0bac51e":{"InputName":"WE78e0bac51e"}}';
